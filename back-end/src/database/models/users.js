3fc5bb26d07b649beef13bfc700a5616a28f4d81
Marcio Junior <marcio.mj70@gmail.com>
2022-07-11 16:38:03 -0300

    feat(database): users model and migration

---
+++ back-end/src/database/models/users.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class users extends Model {}
+
+  users.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {
+    sequelize,
+    modelName: 'users',
+    timestamps: false
+  });
+  return users;
+};
