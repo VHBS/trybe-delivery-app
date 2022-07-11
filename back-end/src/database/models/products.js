e9d9b08f290ba44f62c575617691f63094f277a6
Marcio Junior <marcio.mj70@gmail.com>
2022-07-11 17:34:07 -0300

    feat(database): products seeders

--- back-end/src/database/models/products.js
+++ back-end/src/database/models/products.js
@@ -27,7 +27,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'products',
-    underscored: true
+    underscored: true,
+    timestamps: false,
   });
   return products;
 };
