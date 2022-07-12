50ab29c78a98ad582d609c3ee26c8386d3f1e2f6
Gustavo <gu.bressan@outlook.com>
2022-07-12 16:21:19 -0300

    requisito 1 ate 3, tela de login pronta

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-
 import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './pages/login';
+import Login from './pages/Login/login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Delivery App!</h1>
-      <Routes>
-        <Route exact path="/" element={ <Navigate to="/login" /> } />
-        <Route path="login" element={ <Login /> } />
-      </Routes>
-    </div>
+    <Routes>
+      <Route exact path="/" element={ <Navigate to="/login" /> } />
+      <Route path="login" element={ <Login /> } />
+    </Routes>
   );
 }
 
--- front-end/src/pages/login.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Login = () => <h1>login</h1>;
-
-export default Login;
