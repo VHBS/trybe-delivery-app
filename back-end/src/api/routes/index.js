f8ae243dd7d6fcd8b8c102c86568c6195ab9dbe6
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-07-12 22:49:45 -0300

    feat: add login router at routes

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -3,5 +3,6 @@ const { Router } = require('express');
 const routes = Router();
 
 routes.use('/users', require('./users.routes'));
+routes.use('/login', require('./login.routes'));
 
 module.exports = routes;
