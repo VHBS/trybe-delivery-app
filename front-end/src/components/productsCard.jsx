98f6e439437f82a0399f64dcd32b3e1f39c38037
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-07-14 18:21:22 -0300

    refactor: data test ids fixes

--- front-end/src/components/productsCard.jsx
+++ front-end/src/components/productsCard.jsx
@@ -1,53 +1,56 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+// import useProducts from '../hooks/useProducts';
 
 function ProductsCard() {
+  // const { handleProducts, products } = useProducts();
   const [products, setProducts] = useState([]);
-  const url = 'http://localhost:3001/customer/products';
+  const url = 'http://localhost:3001/products';
 
   useEffect(() => {
     try {
+    // handleProducts();
       axios.get(url).then((response) => {
-        setProducts(response.products);
+        setProducts(response.data);
       });
     } catch (error) {
       throw new Error(error);
     }
-  }, [products]);
+  }, []);
 
   const productsMap = products.map((product) => (
     <div key={ product.id }>
-      <li data-testid="customer_products__element-card-title-">
-        nome:
+      <h3 data-testid={ `customer_products__element-card-title-${product.id}` }>
         {product.name}
-      </li>
-      <li data-testid="customer_products__element-card-price-">
-        preço:
+      </h3>
+      <p data-testid={ `customer_products__element-card-price-${product.id}` }>
         {product.price}
-      </li>
-      <li data-testid="customer_products__img-card-bg-image-">
-        imagem:
-        <img src={ product.image } alt="card-bg" />
-      </li>
-      <li>
-        <button
-          type="button"
-          value="add-btn"
-          data-testid="customer_products__button-card-add-item-"
-        >
-          Adicionar
-        </button>
-      </li>
-      <li>
-        <button
-          type="button"
-          value="rm-btn"
-          data-testid="customer_products__button-card-rm-item-"
-        >
-          Remover
-        </button>
-      </li>
+      </p>
+      <img
+        data-testid={ `customer_products__img-card-bg-image-${product.id}` }
+        src={ product.urlImage }
+        alt="card-bg"
+      />
+      <button
+        type="button"
+        value="add-btn"
+        data-testid={ `customer_products__button-card-add-item-${product.id}` }
+      >
+        +
+      </button>
+      <input
+        type="number"
+        min="0"
+        data-testid={ `customer_products__input-card-quantity-${product.id}` }
+      />
+      <button
+        type="button"
+        value="rm-btn"
+        data-testid={ `customer_products__button-card-rm-item-${product.id}` }
+      >
+        -
+      </button>
     </div>
   ));
 
