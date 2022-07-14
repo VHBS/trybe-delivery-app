db4ca1f84d58b7bdaaacf93e653a63f7a6b6551c
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-07-14 17:24:46 -0300

    refactor: change name rule

--- back-end/src/utils/userValidation.js
+++ back-end/src/utils/userValidation.js
@@ -1,7 +1,7 @@
 const yup = require('yup');
 
 const userSchema = yup.object().shape({
-  name: yup.string().max(12).required(),
+  name: yup.string().min(12).required(),
   email: yup.string().email().required(),
   password: yup.string().min(6).required(),
 });
