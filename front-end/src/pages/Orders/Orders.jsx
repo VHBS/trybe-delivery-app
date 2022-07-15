5237591944def7e124afad6eb6ec3a84f8468764
jpviegas <jp.viegas@hotmail.com>
2022-07-15 20:37:10 -0300

    feat: order page

---
+++ front-end/src/pages/Orders/Orders.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ClientNavBar from '../../components/NavBar';
+
+function Orders() {
+  return (
+    <>
+      <ClientNavBar />
+      <h1>orders</h1>
+    </>
+  );
+}
+
+export default Orders;
