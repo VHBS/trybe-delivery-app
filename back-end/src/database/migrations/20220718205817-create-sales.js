3a9a1844e0cf38895a17cd0b95647c232600bdec
Marcio Junior <marcio.mj70@gmail.com>
2022-07-18 18:29:18 -0300

    fix: add underscore to fields

--- back-end/src/database/migrations/20220718205817-create-sales.js
+++ back-end/src/database/migrations/20220718205817-create-sales.js
@@ -11,6 +11,7 @@ module.exports = {
       userId: {
         allowNull: false,
         type: Sequelize.INTEGER,
+        field: 'user_id',
         references: {
           model: 'users',
           key: 'id'
@@ -21,6 +22,7 @@ module.exports = {
       sellerId: {
         allowNull: false,
         type: Sequelize.INTEGER,
+        field: 'seller_id',
         references: {
           model: 'users',
           key: 'id',
@@ -30,19 +32,23 @@ module.exports = {
       },
       totalPrice: {
         allowNull: false,
-        type: Sequelize.DECIMAL(9, 2)
+        type: Sequelize.DECIMAL(9, 2),
+        field: 'total_price'
       },
       deliveryAddress: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        field: 'delivery_address'
       },
       deliveryNumber: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        field: 'delivery_number'
       },
       saleDate: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'sale_date'
       },
       status: {
         type: Sequelize.STRING
