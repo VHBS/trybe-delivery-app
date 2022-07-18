ccc5964be75d26b6a1b7698608075ad2408a0292
jpviegas <jp.viegas@hotmail.com>
2022-07-18 19:18:00 -0300

    style: checkout button fixed

--- front-end/src/components/CheckoutButton.jsx
+++ front-end/src/components/CheckoutButton.jsx
@@ -1,23 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useCart from '../hooks/useCart';
+import './CheckoutButton.css';
 
 export default function CheckoutButton() {
   const navigate = useNavigate();
-  const { productsCart } = useCart();
-  const [totalCartValue, setTotalCartValue] = useState(0);
-
-  useEffect(() => {
-    const totalValue = productsCart.reduce((acc, curr) => {
-      acc += curr.price * curr.quantity;
-      return acc;
-    }, 0);
-
-    setTotalCartValue(totalValue);
-  }, [productsCart]);
+  const { totalCartValue } = useCart();
 
   return (
     <button
+      className="checkout-btn"
       data-testid="customer_products__button-cart"
       type="button"
       onClick={ () => navigate('../../customer/checkout') }
