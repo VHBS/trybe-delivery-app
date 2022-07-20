41042efdd229dc3b0618c5cf4dd9f6f41afd65f2
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-07-20 18:52:28 -0300

    feat: get sales bu seller id route

--- back-end/src/api/routes/sales.routes.js
+++ back-end/src/api/routes/sales.routes.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
 const createSaleFactory = require('../modules/sales/createSale/CreateSaleFactory');
 const getSaleByIdFactory = require('../modules/sales/getSaleById/GetSaleByIdFactory');
+const getSalesBySellerIdFactory = require(
+    '../modules/sales/getSalesBuSellerId/GetSalesBySellerIdFactory',
+  );
 const 
   getSalesByUserIdFactory = require('../modules/sales/getSalesByUserId/GetSalesByUserIdFactory');
 
@@ -9,5 +12,6 @@ const router = Router();
 router.post('/', (req, res) => createSaleFactory().handle(req, res));
 router.get('/:saleId', (req, res) => getSaleByIdFactory().handle(req, res));
 router.get('/user/:userId', (req, res) => getSalesByUserIdFactory().handle(req, res));
+router.get('/seller/:sellerId', (req, res) => getSalesBySellerIdFactory().handle(req, res));
 
 module.exports = router;
